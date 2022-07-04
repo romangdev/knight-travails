f4aeb4ced49ce5a45acf2598c0a250786a2e656d
Roman Geathers <romangeathersdev@gmail.com>
2022-07-04 11:31:46 -0400

    Organize code into seperate files

---
+++ board.rb
@@ -0,0 +1,18 @@
+# This class is used to generate a visual chess board for the user's reference
+class Board
+  attr_accessor :board_arr, :possible_moves
+  
+  def initialize 
+    @board_arr = []
+    8.times {@board_arr << [0, 1, 2, 3, 4, 5, 6, 7]}
+  end
+
+  # Generates an 8 x 8 chess board numbered with indices from 0 - 7
+  def generate_board
+    count = 7
+    while count >= 0
+      print "Row #{count}: #{@board_arr[count]}\n"
+      count -= 1
+    end
+  end
+end
---
+++ node.rb
@@ -0,0 +1,12 @@
+# Node class is used to generate each visited chess square as a node with attributes to be saved
+# so that we can traverse a tree of nodes, allowing us to find the shortest path a knight can take
+# from start to finish
+class Node
+  attr_accessor :data, :children, :previously_visited
+
+  def initialize(data, previously_visited = [])
+    @data = data
+    @children = nil
+    @previously_visited = previously_visited
+  end
+end
